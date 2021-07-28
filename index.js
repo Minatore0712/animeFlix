9b44e5c11646fd81f0bec14746d6769639a64e9f
Minatore0712 <buesra.sahin97@web.de>
2021-07-28 16:24:40 +0200

    add url to allowed origin

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.listen(port, "0.0.0.0", () => {
   console.log("Listening on Port " + port);
 });
 
-let allowedOrigins = ["http://localhost:7070", "http://localhost:1234", "https://anime-flix-reloaded.netlify.app"];
+let allowedOrigins = ["http://localhost:7070", "http://localhost:1234", "http://localhost:4200", "https://anime-flix-reloaded.netlify.app"];
 
 app.use(
   cors({
